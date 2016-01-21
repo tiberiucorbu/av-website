bb3fddb336dc36ee65c5ec4cf5a7f93739f3c361
Tiberiu CORBU <tiberiu.corbu@geniuxconsulting.de>
2016-01-21 09:54:21 +0100

    Updated slideshow on story page

--- main/static/src/script/public/app.js
+++ main/static/src/script/public/app.js
@@ -1,6 +1,6 @@
 (function(w, $) {
   "use strict";
-
+  var ga = window.ga || function(){};
   var initSlideshow = function(el) {
     var slideShow = new w.Slideshow(el);
     $(el).data('slideshowInstance', slideShow);
@@ -8,7 +8,6 @@
 
   var decorateSlideshowElements = function(){
     $('[data-target="slideshow"]').each(function(idx, el) {
-      console.log(el);
       if (!$(el).data('deffer-load') && !$(el).data('slideshowInstance')) {
         initSlideshow($(el));
       }
@@ -23,12 +22,13 @@
 
     var $swiperModalEl = $('#swiper-modal-container');
 
-    $swiperModalEl.modal('hide'); // init the modal
+    $swiperModalEl.modal('hide');
 
 
     $('[data-target="open-modal-swiper"]').each(function(idx, el) {
       var $el = $(el);
       var href = $el.attr('href');
+
       if (href) {
         $el.on('click', function(e) {
           e.preventDefault();
@@ -46,7 +46,9 @@
               window.setTimeout(function(){
                 decorateSlideshowElements();
                 $swiperModalEl.find('.close').on('click', function(){
+                    e.preventDefault();
                     $swiperModalEl.modal('hide');
+                    ga('send', 'event', 'Story', 'Slideshow', 'Close Modal', 'close' );
                 });
               }, 500);
               $swiperModalEl.modal('show');
@@ -60,18 +62,25 @@
     });
   });
 
+$(function(){
+    $('a').on('click', function(){
+      ga('send', 'event', 'Navigation', 'Link', $(this).text(), $(this).attr('href'));
+    });
+    $('#hp-carousel').on('slide.bs.carousel', function () {
+      ga('send', 'event', 'Navigation', 'Link', $(this).text(), $(this).attr('href'));
+    });
+});
+
   $(function() {
 
     $('[data-toggle="menu"]').each(function(idx, el) {
       var $el = $(el);
 
       var toggle = function(e){
-
             $el.parent().toggleClass('open');
             $el.find('.caret').toggleClass('up');
+            ga('send', 'event', 'Navigation', 'Toggle', el.parent().text(), el.parent().text());
             e.preventDefault();
-
-
 
       };
       $el.on('click', toggle);
